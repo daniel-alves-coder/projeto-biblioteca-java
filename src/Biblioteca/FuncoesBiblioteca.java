7d92428b0b3f1031f072c628d1044cfadd4786d4
Daniel <daniel.alves.costa2006@gmail.com>
2025-05-20 11:48:29 -0300

    Primeira versão da biblioteca

---
+++ src/Biblioteca/FuncoesBiblioteca.java
@@ -0,0 +1,70 @@
+package Biblioteca;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class FuncoesBiblioteca {
+
+    public static void exibirMenu() {
+        System.out.println("____________________________");
+        System.out.println("MENU");
+        System.out.println("____________________________");
+        System.out.println("1 - Adicionar");
+        System.out.println("2 - Pesquisar");
+        System.out.println("3 - Listar");
+        System.out.println("4 - Excluir");
+        System.out.println("5 - Sair");
+        System.out.println("____________________________");
+    }
+
+    public static void adicionar(HashMap<String, ArrayList> listaLivros, String autor, String obra) {
+        if (listaLivros.containsKey(autor)) {
+            for (ArrayList<String> elementos : listaLivros.values()) {
+                elementos.add(obra);
+            }
+        } else {
+            ArrayList<String> livro = new ArrayList<>();
+            listaLivros.put(autor, livro);
+            livro.add(obra);
+        }
+    }
+
+    public static void listar(HashMap<String, ArrayList> listaLivros) {
+        for (String autor : listaLivros.keySet()) {
+            for (Object livro : listaLivros.get(autor)) {
+                System.out.println(livro + " - " + autor);
+            }
+        }
+    }
+
+    public static void pesquisar(HashMap<String, ArrayList> listaLivros, String titulo) {
+        boolean encontrado = false;
+        for (String autor : listaLivros.keySet()) {
+            for (Object livro : listaLivros.get(autor)) {
+                if (livro.equals(titulo)) {
+                    encontrado = true;
+                    System.out.println("____________________________");
+                    System.out.println("Livro encotrado!");
+                    System.out.println("NOME : " + livro);
+                    System.out.println("AUTOR: " + autor);
+                }
+            }
+        }
+        if (!encontrado) {
+            System.out.println("____________________________");
+            System.out.println("Esse livro não foi encontrado!");
+        }
+
+    }
+
+    public static boolean excluir(HashMap<String, ArrayList> listaLivros, String titulo) {
+        boolean livroRemovido = false;
+        for (ArrayList livros : listaLivros.values()) {
+            if (livros.contains(titulo)) {
+                livroRemovido = true;
+                livros.remove(titulo);
+            }
+        }
+        return livroRemovido;
+    }
+}
---
+++ src/Biblioteca/Main.java
@@ -0,0 +1,62 @@
+package Biblioteca;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        HashMap<String, ArrayList> livros = new HashMap<>();
+        int opcao = 0;
+        String autor;
+        String obra;
+        String titulo;
+
+        while (opcao != 5) {
+            FuncoesBiblioteca.exibirMenu();
+            System.out.print("Digite a opção: ");
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    System.out.print("Digite o nome do autor: ");
+                    autor = scanner.nextLine();
+                    System.out.print("Digite o nome da obra: ");
+                    obra = scanner.nextLine();
+
+                    FuncoesBiblioteca.adicionar(livros, autor, obra);
+                    System.out.println("Livro adicionado com sucesso!");
+                    break;
+                case 2:
+                    System.out.print("Digite o titulo da obra: ");
+                    titulo = scanner.nextLine();
+                    FuncoesBiblioteca.pesquisar(livros, titulo);
+                    break;
+                case 3:
+                    FuncoesBiblioteca.listar(livros);
+                    break;
+                case 4:
+                    System.out.print("Digite o titulo da obra: ");
+                    titulo = scanner.nextLine();
+                    boolean livroRemovido = FuncoesBiblioteca.excluir(livros, titulo);
+
+                    if (livroRemovido){
+                        System.out.println("Livro removido com sucesso!");
+                    }else{
+                        System.out.println("Livro não encotrado!");
+                    }
+                    break;
+                case 5:
+                    System.out.println("Saindo do sistema...");
+                    break;
+                default:
+                    System.out.println("Opção Invalida!, Digite novamente.");
+                    break;
+            }
+        }
+        scanner.close();
+    }
+}
